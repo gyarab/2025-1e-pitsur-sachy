2a7b3086f6cd8b0af5977a250fabc925a2939357
Nazarii <nazarii.pitsur.s@gyarab.cz>
2026-04-29 18:04:04 +0200

    Update Vez.java
    uprava vez

--- src/main/java/ProjektSachy/Vez.java
+++ src/main/java/ProjektSachy/Vez.java
@@ -3,63 +3,66 @@ package ProjektSachy;
 import java.util.ArrayList;
 import java.util.List;
 
-public  class Vez extends Figurka {
+public class Vez extends Figurka {
 
     public Vez(boolean jeBila) {
         this.jeBila = jeBila;
     }
+
     public boolean pohnulSe = false;
 
     @Override
-    public List<Pozice> MozneTahy(Figurka[][] Sachovnice, int x,  int y) {
+    public List<Pozice> MozneTahy(Figurka[][] Sachovnice, int x, int y) {
         List<Pozice> tahy = new ArrayList<>();
 
-        //hahoru
-        for (int i=x-1; i>=0; i--) {
-            if (Sachovnice[i][y]==null) {
+        // nahoru
+        for (int i = x - 1; i >= 0; i--) {
+            if (Sachovnice[i][y] == null) {
                 tahy.add(new Pozice(i, y));
-            }else{
-                if(Sachovnice[i][y].jeBila != this.jeBila){
-                    tahy.add(new Pozice(i, y));
-                }
-                break;
-            }
-        }
-        //dolu
-        for (int i=x+1; i<8; i++) {
-            if (Sachovnice[i][y]==null) {
-                tahy.add(new Pozice(i, y));
-            }else{
-                if (Sachovnice[i][y].jeBila != this.jeBila){
+            } else {
+                if (Sachovnice[i][y].jeBila != this.jeBila) {
                     tahy.add(new Pozice(i, y));
                 }
                 break;
             }
         }
 
-        //vlevo
-        for (int i=y-1; i>=0; i--) {
-            if (Sachovnice[i][x]==null) {
-                tahy.add(new Pozice(i, x));
-            }else{
-                if (Sachovnice[i][x].jeBila != this.jeBila){
-                    tahy.add(new Pozice(i, x));
+        // dolu
+        for (int i = x + 1; i < 8; i++) {
+            if (Sachovnice[i][y] == null) {
+                tahy.add(new Pozice(i, y));
+            } else {
+                if (Sachovnice[i][y].jeBila != this.jeBila) {
+                    tahy.add(new Pozice(i, y));
                 }
                 break;
             }
         }
 
-        //vpravo
-        for (int i=y+1; i<8; i++) {
-            if (Sachovnice[x][i]==null) {
-            }else{
-                if (Sachovnice[x][i].jeBila != this.jeBila){
+        // vlevo
+        for (int i = y - 1; i >= 0; i--) {
+            if (Sachovnice[x][i] == null) {
+                tahy.add(new Pozice(x, i));
+            } else {
+                if (Sachovnice[x][i].jeBila != this.jeBila) {
                     tahy.add(new Pozice(x, i));
                 }
                 break;
             }
         }
-        return tahy;
 
+        // vpravo
+        for (int i = y + 1; i < 8; i++) {
+            if (Sachovnice[x][i] == null) {
+                tahy.add(new Pozice(x, i));
+            } else {
+                if (Sachovnice[x][i].jeBila != this.jeBila) {
+                    tahy.add(new Pozice(x, i));
+                }
+                break;
+            }
+        }
+
+        return tahy;
     }
 }
