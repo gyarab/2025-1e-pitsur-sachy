d474d4f88bdf14e5fbe2872b2236c36e58f85978
Nazarii <nazarii.pitsur.s@gyarab.cz>
2026-05-01 09:44:17 +0200

    Oprava enpassantu

--- src/main/java/ProjektSachy/Sachovnice.java
+++ src/main/java/ProjektSachy/Sachovnice.java
@@ -49,95 +49,105 @@ public class Sachovnice {
         Pesec.sachovnice = this;
     }
 
-    public boolean provedTah(int x1, int y1, int x2, int y2) {
-        Figurka f = policko[x1][y1];
+   public boolean provedTah(int x1, int y1, int x2, int y2) {
+    Figurka f = policko[x1][y1];
 
-        // neni tam figurka
-        if (f == null) return false;
+    // neni tam figurka
+    if (f == null) return false;
 
-        // spatny hrač na tahu
-        if (f.jeBila != bilyHraji) return false;
+    // spatny hrac na tahu
+    if (f.jeBila != bilyHraji) return false;
 
-        // vezmi mozne tahy
-        var tahy = f.MozneTahy(policko, x1, y1);
+    // vezmi mozne tahy
+    var tahy = f.MozneTahy(policko, x1, y1);
 
-        // zkontroluj jestli tam muze jit
-        for (Pozice p : tahy) {
-            if (p.x == x2 && p.y == y2) {
+    // zkontroluj jestli tam muze jit
+    for (Pozice p : tahy) {
+        if (p.x == x2 && p.y == y2) {
 
-                // ulozime co je na cilovem poli
-                Figurka cil = policko[x2][y2];
+            // ulozime co je na cilovem poli
+            Figurka cil = policko[x2][y2];
 
-                // provedeni tahu
-                policko[x2][y2] = f;
-                policko[x1][y1] = null;
+            // provedeni tahu nanecisto
+            policko[x2][y2] = f;
+            policko[x1][y1] = null;
 
-                // kontrola jestli jsme si nedali sach
-                boolean sach = jeSach(f.jeBila);
+            // kontrola jestli jsme si nedali sach
+            boolean sach = jeSach(f.jeBila);
 
-                // vratime tah zpet
-                policko[x1][y1] = f;
-                policko[x2][y2] = cil;
+            // vratime tah zpet
+            policko[x1][y1] = f;
+            policko[x2][y2] = cil;
 
-                // pokud je sach tah je neplatny
-                if (sach) return false;
+            // pokud je sach tah je neplatny
+            if (sach) return false;
 
-                // provedeni tahu
-                policko[x2][y2] = f;
-                policko[x1][y1] = null;
+            // provedeni tahu natvrdo
+            policko[x2][y2] = f;
+            policko[x1][y1] = null;
 
-                // nastaveni ze se figurka pohnula
-                if (f instanceof Kral) {
-                    ((Kral) f).pohnulSe = true;
+            // en passant sebrani
+            if (f instanceof Pesec) {
+                if (y1 != y2 && cil == null) {
+                    policko[x1][y2] = null;
                 }
-
-                if (f instanceof Vez) {
-                    ((Vez) f).pohnulSe = true;
-                }
-
-                // rosada posun veze
-                if (f instanceof Kral) {
-
-                    // kratka rosada
-                    if (y2 == y1 + 2) {
-                        Figurka vez = policko[x1][7];
-                        policko[x1][5] = vez;
-                        policko[x1][7] = null;
-                    }
-
-                    // dlouha rosada
-                    if (y2 == y1 - 2) {
-                        Figurka vez = policko[x1][0];
-                        policko[x1][3] = vez;
-                        policko[x1][0] = null;
-                    }
-                }
-
-                // nastaveni en passant
-                if (f instanceof Pesec) {
-                    if (Math.abs(x2 - x1) == 2) {
-                        enPassantX = (x1 + x2) / 2;
-                        enPassantY = y1;
-                    }
-                }
-
-                // promotion pesce
-                if (f instanceof Pesec) {
-                    if ((f.jeBila && x2 == 0) || (!f.jeBila && x2 == 7)) {
-                        policko[x2][y2] = new Dama(f.jeBila);
-                    }
-                }
-
-                // zmena hrace
-                bilyHraji = !bilyHraji;
-
-                return true;
             }
-        }
 
-        return false;
+            // reset en passant
+            enPassantX = -1;
+            enPassantY = -1;
+
+            // nastaveni ze se figurka pohnula
+            if (f instanceof Kral) {
+                ((Kral) f).pohnulSe = true;
+            }
+
+            if (f instanceof Vez) {
+                ((Vez) f).pohnulSe = true;
+            }
+
+            // rosada posun veze
+            if (f instanceof Kral) {
+
+                // kratka rosada
+                if (y2 == y1 + 2) {
+                    Figurka vez = policko[x1][7];
+                    policko[x1][5] = vez;
+                    policko[x1][7] = null;
+                }
+
+                // dlouha rosada
+                if (y2 == y1 - 2) {
+                    Figurka vez = policko[x1][0];
+                    policko[x1][3] = vez;
+                    policko[x1][0] = null;
+                }
+            }
+
+            // nastaveni en passant
+            if (f instanceof Pesec) {
+                if (Math.abs(x2 - x1) == 2) {
+                    enPassantX = (x1 + x2) / 2;
+                    enPassantY = y1;
+                }
+            }
+
+            // promotion pesce
+            if (f instanceof Pesec) {
+                if ((f.jeBila && x2 == 0) || (!f.jeBila && x2 == 7)) {
+                    policko[x2][y2] = new Dama(f.jeBila);
+                }
+            }
+
+            // zmena hrace
+            bilyHraji = !bilyHraji;
+
+            return true;
+        }
     }
 
+    return false;
+}
     public boolean jeSach(boolean bila) {
 
         int kralX = -1;
